0947f04ecd474f5d9178a9a524890d4a372e4c61
liuanjiu <3026360980@qq.com>
2017-09-11 14:29:59 +0800

    初始化的项目

--- newProject/src/app/app.module.ts
+++ newProject/src/app/app.module.ts
@@ -5,11 +5,10 @@ import { AppComponent } from './app.component';
 import { RegistComponent } from './regist/regist.component';
 import { IndexComponent } from './index/index.component';
 import { ProprietaryCustomizationComponent } from './proprietary-customization/proprietary-customization.component';
-import { SerciceComponent } from './sercice/sercice.component';
+import { ServicePackageComponent } from './service-package/service-package.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
 import { PersonalCenterComponent } from './personal-center/personal-center.component';
-import { ServicePackageComponent } from './service-package/service-package.component';
 import { RealCaseComponent } from './real-case/real-case.component';
 import { ReserveComponent } from './reserve/reserve.component';
 import { DetailsComponent } from './details/details.component';
@@ -20,11 +19,10 @@ import { DetailsComponent } from './details/details.component';
     RegistComponent,
     IndexComponent,
     ProprietaryCustomizationComponent,
-    SerciceComponent,
+    ServicePackageComponent,
     LoginComponent,
     PageNotfoundComponent,
     PersonalCenterComponent,
-    ServicePackageComponent,
     RealCaseComponent,
     ReserveComponent,
     DetailsComponent
